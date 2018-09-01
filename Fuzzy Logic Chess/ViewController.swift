e4e685fdbf08f64b1d9bd4a9b118da2d919d7c9c
illyab2009 <illya.b2009@gmail.com>
2018-09-01 09:58:10 -0400

    Initial Commit

---
+++ Fuzzy Logic Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fuzzy Logic Chess
+//
+//  Created by Illya Balakin on 9/1/18.
+//  Copyright © 2018 KSU CS Seniors Project 6A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
