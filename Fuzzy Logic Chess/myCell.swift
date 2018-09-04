0ef8d0f9ecf8d2efdec6a45babe2104548c8bd62
illyab2009 <illya.b2009@gmail.com>
2018-09-04 09:13:15 -0400

    initial commit

---
+++ Fuzzy Logic Chess/myCell.swift
@@ -0,0 +1,16 @@
+//
+//  myCell.swift
+//  Fuzzy Logic Chess
+//
+//  Created by Illya Balakin on 9/2/18.
+//  Copyright © 2018 KSU CS Seniors Project 6A. All rights reserved.
+//
+
+import UIKit
+
+class myCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+	@IBOutlet weak var myLabel: UILabel!
+	
+}
