98ac0c39723d836c3c304898b67d0f173d6779ad
Nick Curtin <36422581+NickCurtin@users.noreply.github.com>
2018-09-24 14:35:17 -0700

    Graveyard Text Addition
    I added the graveyard text holders for future graveyard. Also combined the asset folders.

---
+++ Fuzzy-Logic-Chess/Fuzzy Logic Chess/MenuTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MenuTableViewCell.swift
+//  Fuzzy Logic Chess
+//
+//  Created by Pouya Ranjbar on 9/21/18.
+//  Copyright © 2018 KSU CS Seniors Project 6A. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var label_title: UILabel!
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
