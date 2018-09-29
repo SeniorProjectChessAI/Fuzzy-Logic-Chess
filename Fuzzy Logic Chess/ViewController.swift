b777986dd175d16b49ca2318b782c42f9c0655fd
illyab2009 <illya.b2009@gmail.com>
2018-09-29 12:10:20 -0400

    Added move piece to empty cell functionality

--- Fuzzy Logic Chess/ViewController.swift
+++ Fuzzy Logic Chess/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate {
 	//show the menu 
 	
-    @IBOutlet weak var myCollectionView: UICollectionView!
-	@IBOutlet weak var die_imageView: UIImageView!
 	
-	var board: Board!
+    
+    
+    @IBOutlet weak var board: Board!
+    @IBOutlet weak var die_imageView: UIImageView!
+	
+	//var board: Board!
 	
 	let light = UIColor.init(displayP3Red: 142.0/255.0, green: 109.0/255.0, blue: 67/255.0, alpha: 1.0)
 	let dark = UIColor.init(displayP3Red: 54.0/255.0, green: 38.0/255.0, blue: 19.0/255.0, alpha: 1.0)
@@ -28,11 +31,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 	
 	var menu_vc : MenuViewController!
 	
+	// variables for selected/clicked cells
+	var tileIsSelected: Bool = false
+	var previouslySelectedTileColor: UIColor?
+	var previouslySelectedTile: Int?
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		
-		
 		
 		menu_vc = self.storyboard?.instantiateViewController(withIdentifier: "MenuViewController") as? MenuViewController
 		
@@ -47,47 +53,51 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 		layout.minimumInteritemSpacing = 0
 		layout.minimumLineSpacing = 0
 		
-		myCollectionView.collectionViewLayout = layout
+		board.collectionViewLayout = layout
+
+	
+		board.setup()
 		
-		board = Board()
-		
-		print("view did load")
-		
-		myCollectionView.reloadData()
-		myCollectionView.performBatchUpdates(nil, completion: {
-			(result) in
-			self.setNewBoard()
-		})
+//		board.reloadData()
+//		board.performBatchUpdates(nil, completion: {
+//			(result) in
+//			self.setNewBoard()
+//		})
 		
 		rollDie() // Rolls die for presentation purposes..
     }
 	
-	func setNewBoard() {
-		// set default black pieces
-		for piece in board.blackPieces {
-			print("black index: \(piece.defaultLocation)")
-			let tile = myCollectionView.cellForItem(at: IndexPath(row: piece.defaultLocation, section: 0)) as! Tile
-			tile.foregroundImageView.image = UIImage(named: piece.imageName)
-		}
-		
-		// set default white pieces
-		for piece in board.whitePieces {
-			print("white index: \(piece.defaultLocation)")
-			let tile = myCollectionView.cellForItem(at: IndexPath(row: piece.defaultLocation, section: 0)) as! Tile
-			tile.foregroundImageView.image = UIImage(named: piece.imageName)
-		}
-	}
+//	func setNewBoard() {
+//		// set default black pieces
+//		for piece in board.blackPieces {
+//			print("black index: \(piece.location)")
+//			let tile = board.cellForItem(at: IndexPath(row: piece.location, section: 0)) as! Tile
+//			tile.foregroundImageView.image = UIImage(named: piece.imageName)
+//		}
+//
+//		// set default white pieces
+//		for piece in board.whitePieces {
+//			print("white index: \(piece.location)")
+//			let tile = board.cellForItem(at: IndexPath(row: piece.location, section: 0)) as! Tile
+//			tile.foregroundImageView.image = UIImage(named: piece.imageName)
+//		}
+//	}
 
     
     // Number of views in the collectionView
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 64 // 8 x 8 board
-    }
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 64
+	}
 
     // Populate views
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let tile = collectionView.dequeueReusableCell(withReuseIdentifier: "tile", for: indexPath) as! Tile
-        //tile.backgroundImageView.image = UIImage(named: "images/d1.png")
+		
+		tile.location = indexPath.row
+		
+		tile.piece = board.getPieceAtLocation(location: indexPath.row)
+		
+		tile.setPiece(piece: board.getPieceAtLocation(location: indexPath.row))
 		
 		
 		
@@ -117,28 +127,43 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 		
 		if indexPath.row % 2 == 0 {
 			tile.backgroundColor = evenColor
-			
-			//tile.backgroundImageView.image = UIImage(named: ("images/cells/\(evenCode)\(arc4random_uniform(8) + 1).png"))
-			
 		} else {
 			tile.backgroundColor = oddColor
-			
-			//tile.backgroundImageView.image = UIImage(named: ("images/cells/\(oddCode)\(arc4random_uniform(8) + 1).png"))
 		}
 		
 		return tile
     }
 	
+	
 	// Called when tile is clicked
 	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		print("Tile clicked: \(indexPath.row)")
 		
-		//let indexPath2 = IndexPath(item: indexPath.row - 8, section: 0);
+		let tile = board.cellForItem(at: indexPath) as! Tile
 		
-		collectionView.cellForItem(at: indexPath)?.backgroundColor = UIColor.black
-		
-		
+		if(!tileIsSelected && tile.hasPiece()) {
+			previouslySelectedTileColor = tile.backgroundColor
+			tile.backgroundColor = UIColor.cyan
+			tileIsSelected = true;
+			previouslySelectedTile = indexPath.row
+		}
+		else if(tileIsSelected && !tile.hasPiece()) {
+			
+			let previousTile = board.cellForItem(at: IndexPath(row: previouslySelectedTile!, section: 0)) as! Tile
+			
+			// set previously selected piece to newly selected tile
+			tile.setPiece(piece: previousTile.piece)
+				
+			// remove previously selected tile's image and restore original tile color
+			previousTile.removePiece()
+			previousTile.backgroundColor = previouslySelectedTileColor
+			
+			tileIsSelected = false
+			
+			print("piece moved to tile \(indexPath.row) ")
+		}
 	}
+
 	
 	//menu button
 	@IBAction func menu_action(_ sender: UIBarButtonItem) {
