6c54c579c8e3d5a0e543ec5a22ffc7c612b380ba
illyab2009 <illya.b2009@gmail.com>
2018-10-18 13:02:03 -0400

    initial commit

---
+++ Fuzzy Logic Chess/Graveyardl.swift
@@ -0,0 +1,19 @@
+//
+//  GraveyardCell.swift
+//  Fuzzy Logic Chess
+//
+//  Created by Illya Balakin on 10/16/18.
+//  Copyright © 2018 KSU CS Seniors Project 6A. All rights reserved.
+//
+
+import UIKit
+
+
+class Graveyard: UIViewController {
+	
+}
+
+class GraveyardCell: UICollectionViewCell {
+	
+
+}
