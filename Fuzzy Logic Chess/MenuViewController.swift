fcbda5fe485d2782cdc1d8523e10cdfa229b1191
illyab2009 <illya.b2009@gmail.com>
2018-10-25 19:17:00 -0400

    Added functionality that changes color of menu to the selected difficulty color

--- Fuzzy Logic Chess/MenuViewController.swift
+++ Fuzzy Logic Chess/MenuViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
     @IBOutlet weak var menu_tableView: UITableView!
+	
     //This array of names will apear on the menu
 	let title_arr = ["   How to Play ","   New Game"]
 	var myIndex = 0
@@ -18,31 +19,80 @@ class MenuViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-		super.viewDidLoad()
 		
 		// Do any additional setup after loading the view.
 		menu_tableView.delegate = self
 		menu_tableView.dataSource = self
 		
+		setColors()
+		
+		// observes notification of difficultyMenuMessage from ViewController
+		NotificationCenter.default.addObserver(self, selector: #selector(onRecieveDifficulty(_:)), name: Notification.Name(rawValue: "difficultyMenuTableMessage"), object: nil)
 	}
-    
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-	return title_arr.count
-}
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-	let cell = tableView.dequeueReusableCell(withIdentifier: "menu_cell") as!MenuTableViewCell
 	
-	cell.lable_title.text = title_arr[indexPath.row]
-	return cell
-}
+	func setColors() {
+		switch(DIFFICULTY) {
+		case 0: setEasyColor()
+		case 1: setMediumColor()
+		case 2: setHardColor()
+		default: setEasyColor()
+		}
+	}
+	
+	
+	// Used when difficulty changed after viewDidLoad() function
+	@objc func onRecieveDifficulty(_ notification:Notification) {
+		print("RECEIVED TABLE DIFFICULTY")
+		
+		if let data = notification.userInfo as? [Int:Int] {
+			for (_, difficulty) in data {
+				switch(difficulty) {
+				case 0: setEasyColor()
+				case 1: setMediumColor()
+				case 2: setHardColor()
+				default: setEasyColor()
+				}
+			}
+		}
+	}
+	
+	func setEasyColor() {
+		let green = UIColor.init(displayP3Red: 50.0/255.0, green: 77.0/255.0, blue: 46.0/255.0, alpha: 1.0)
+		menu_tableView.backgroundColor = green
+	}
+	
+	func setMediumColor() {
+		let blue = UIColor.init(displayP3Red: 23.0/255.0, green: 42.0/255.0, blue: 83.0/255.0, alpha: 1.0)
+		menu_tableView.backgroundColor = blue
+	}
+	
+	func setHardColor() {
+		let red = UIColor.init(displayP3Red: 83.0/255.0, green: 39.0/255.0, blue: 42.0/255.0, alpha: 1.0)
+		menu_tableView.backgroundColor = red
+	}
+	
+    
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return title_arr.count
+	}
+	
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "menu_cell") as!MenuTableViewCell
+	
+		cell.label_title.text = title_arr[indexPath.row]
+		
+		return cell
+	}
+	
+	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		myIndex = indexPath.row
 		
 		performSegue(withIdentifier: "segue", sender:indexPath.endIndex)
-		
-		
 	}
+	
+	
 	func changeView(){
 		let storyboard = UIStoryboard(name: "Start", bundle: nil)
 		//let nc = storyboard.instantiateViewController(withIdentifier: "fff")
